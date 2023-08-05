a17545253dc0f429be3b5a67741e4d2ef434e93a
Uriel Gartzia <ugartziasola@gmail.com>
2023-08-05 17:17:23 +0200

    falta el css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movies = require("../models/Movie.model.js");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movies.find()
+    //el select filtra las propiedades que quiero imprimir en la terminal para ganar en eficiencia
+    //usa el lenguaje de mongo db
+    .select({ title: 1, image: 1 })
+    .then((allMovies) => {
+      console.log(allMovies);
+      res.render("movies.hbs", { moviesArr: allMovies });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movies.findById({_id: req.params.id})
+  .then((oneMovie) => {
+    console.log(oneMovie)
+    res.render("movie-details.hbs", { movieDetails: oneMovie})
+  })
+  .catch((error) => {
+    next(error)
+  })
+});
 
 module.exports = router;
